d7ddd16cd619709ec81ca4f1cf0470073994980a
smartoctopus <alessandro.bozzato.2007@outlook.it>
2021-12-22 19:07:19 +0100

    Add likely/unlikely/has_flag + formatted macros.h

--- macros.h
+++ macros.h
@@ -8,11 +8,16 @@
 #endif
 
 #if !defined(swap)
-#define swap(type, a, b) do { type tmp = (a); a = b; b = tmp; } while(0)
+#define swap(type, a, b)                                                       \
+  do {                                                                         \
+    type tmp = (a);                                                            \
+    a = b;                                                                     \
+    b = tmp;                                                                   \
+  } while (0)
 #endif
 
 #if !defined(reinterpret_cast)
-#define reinterpret_cast(type, var) *cast(type*)(&var)
+#define reinterpret_cast(type, var) *cast(type *)(&var)
 #endif
 
 #if !defined(countof)
@@ -24,21 +29,23 @@
 #endif
 
 #if !defined(alignof)
-#define alignof(Type) offsetof(struct {char c; Type field; }, field)
+#define alignof(Type)                                                          \
+  offsetof(                                                                    \
+      struct {                                                                 \
+        char c;                                                                \
+        Type field;                                                            \
+      },                                                                       \
+      field)
 #endif
 
 #if !defined(unused)
 #if defined(COMPILER_MSVC)
-#define unused(x) (__pragma(warning(suppress:4100))(x))
+#define unused(x) (__pragma(warning(suppress : 4100))(x))
 #elif defined(GNUC_FEATURES)
-#define unused(x) __attribute__((__unused__))(x)
+#define unused(x) __attribute__((__unused__)) (x)
 #else
 #define unused(x) (void)(sizeof(x))
 #endif
-#endif
-
-#if !defined(local)
-#define local static
 #endif
 
 #if !defined(JOIN_MACROS)
@@ -74,14 +81,16 @@
 #endif
 
 #if !defined(clamp)
-#define clamp(x, lower_bound, higher_bound) min(max((x), (lower_bound)), (higher_bound))
+#define clamp(x, lower_bound, higher_bound)                                    \
+  min(max((x), (lower_bound)), (higher_bound))
 #endif
 
 #if !defined(is_between)
-#define is_between(x, lower_bound, higher_bound) (((lower_bound) <= (x)) && ((x)) <= (upper_bound))
+#define is_between(x, lower_bound, higher_bound)                               \
+  (((lower_bound) <= (x)) && ((x)) <= (upper_bound))
 #endif
 
-#if !defined(abs)
+#if !defined(_abs)
 #define _abs(x) ((x) < 0 ? -(x) : (x))
 #endif
 
@@ -89,6 +98,18 @@
 #define set_flag(flag, bit) ((flag) |= (bit))
 #define toggle_flag(flag, bit) ((flag) ^= (bit))
 #define unset_flag(flag, bit) ((flag) &= ~(bit))
+#define has_flag(flag, bit) ((flag) & (bit))
+#endif
+
+/* Likely/Unlikely */
+#if !defined(likely) && !defined(unlikely)
+#if defined(GNUC_FEATURES)
+#define likely(expr) __builtin_expect(!!(expr), 1)
+#define unlikely(expr) __builtin_expect(!!(expr), 0)
+#else
+#define likely(expr) (!!(expr))
+#define unlikely(expr) (!!(expr))
+#endif
 #endif
 
 #endif /* MACROS_H_ */
